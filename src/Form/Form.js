6cc9a77142d56ef7bc5802e2660551e90a2cbd80
lputra24 <lputra@student.unimelb.edu.au>
2019-12-15 23:36:14 +1100

    First commit Message

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,4 +1,6 @@
 import React, {useState} from 'react'
+import FormValidation from './FormValidation.js'
+import validator from 'validator'
 
 const Form=()=>{
     const [emailState,updateEmailState]=useState({
@@ -17,15 +19,21 @@ const Form=()=>{
     });
 
     const handleInputChange=(event,updateStateFunc)=>{
-        updateStateFunc({
+        event.preventDefault();
+        event.persist();
+        updateStateFunc(prevState=>({
+            ...prevState,
             inputText: event.target.value
-        });
+        }));
+
+        console.log(emailState)
     }
 
     return(
     <form className="loginForm">
         <input type="email" value={emailState.inputText} placeholder='example@gmail.com' onChange={(event)=>handleInputChange(event,updateEmailState)}/>
         <p>{emailState.inputText}</p>
+        <FormValidation formType="EMAIL" inputText={emailState.inputText}/>
         <input type="password" value={passwordState.inputText} onChange={(event)=>handleInputChange(event,updatePasswordState)}/>
         <p>{passwordState.inputText}</p>
         <input type="password" value={passwordConfirmState.inputText} onChange={(event)=>handleInputChange(event,updatePasswordConfirmState)}/>
--- src/Form/FormValidation.js
+++ src/Form/FormValidation.js
@@ -1,2 +1,22 @@
+import React from 'react'
 import validator from 'validator'
 
+const EMAIL="EMAIL"
+
+const FormValidation = (props) => {
+    const validation = (props) => {
+        switch (props.formType){
+            case EMAIL: 
+                return validator.isEmail(props.inputText).toString();
+            default:
+                return "gg"
+        }
+        
+    }
+ 
+    return(
+        <p>this : {validation(props)}</p>
+    );
+};
+
+export default FormValidation;
