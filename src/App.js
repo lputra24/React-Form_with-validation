9236b38865aa72e9ee19d6b06e6865af7498a80b
lputra24 <lputra@student.unimelb.edu.au>
2019-12-16 01:22:56 +1100

    First commit Message

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const App = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Sign Up
         </p>
         <Form/>
       </header>
--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,22 +1,94 @@
-import React, {useState} from 'react'
-import FormValidation from './FormValidation.js'
+import React, {useState, useEffect} from 'react'
+
 import validator from 'validator'
 
 const Form=()=>{
     const [emailState,updateEmailState]=useState({
         isValid: false,
-        inputText: ''
+        inputText: '',
+        errMessage:''
     });
+    useEffect(()=>{
+        if(validator.isEmpty(emailState.inputText)){
+            updateEmailState(prevState=>({
+                ...prevState,
+                isValid:false,
+                errMessage:''
+            }));
+        }
+        else if(validator.isEmail(emailState.inputText)){
+            updateEmailState(prevState=>({
+                ...prevState,
+                isValid:true,
+                errMessage:''
+            }));
+        }
+        else{
+            updateEmailState(prevState=>({
+                ...prevState,
+                isValid:false,
+                errMessage:"not a valid email address"
+            }));
+        }
+        
+    },[emailState.inputText]);
 
     const [passwordState,updatePasswordState]=useState({
         isValid: false,
-        inputText: ''
+        inputText: '',
+        errMessage:''
     });
+    useEffect(()=>{
+        if(validator.isEmpty(passwordState.inputText)){
+            updatePasswordState(prevState=>({
+                ...prevState,
+                isValid: false,
+                errMessage: ''
+            }));
+        }
+        else if(passwordState.inputText.length<8){
+            updatePasswordState(prevState=>({
+                ...prevState,
+                isValid: false,
+                errMessage: 'Password must be at least 8 characters'
+            }));
+        }
+        else {
+            updatePasswordState(prevState=>({
+                ...prevState,
+                isValid: true,
+                errMessage: ''
+            }));
+        }
+    },[passwordState.inputText]);
 
     const [passwordConfirmState,updatePasswordConfirmState]=useState({
         isValid: false,
         inputText: ''
     });
+    useEffect(()=>{
+        if(validator.isEmpty(passwordConfirmState.inputText)){
+            updatePasswordConfirmState(prevState=>({
+                ...prevState,
+                isValid: false,
+                errMessage: ''
+            }));
+        }
+        else if(passwordState.inputText!==passwordConfirmState.inputText){
+            updatePasswordConfirmState(prevState=>({
+                ...prevState,
+                isValid: false,
+                errMessage: 'Password does not match'
+            }));
+        }
+        else {
+            updatePasswordConfirmState(prevState=>({
+                ...prevState,
+                isValid: true,
+                errMessage: ''
+            }));
+        }
+    },[passwordConfirmState.inputText,passwordState.inputText]);
 
     const handleInputChange=(event,updateStateFunc)=>{
         event.preventDefault();
@@ -25,19 +97,21 @@ const Form=()=>{
             ...prevState,
             inputText: event.target.value
         }));
-
-        console.log(emailState)
     }
+
 
     return(
     <form className="loginForm">
         <input type="email" value={emailState.inputText} placeholder='example@gmail.com' onChange={(event)=>handleInputChange(event,updateEmailState)}/>
-        <p>{emailState.inputText}</p>
-        <FormValidation formType="EMAIL" inputText={emailState.inputText}/>
+        {/* <p>{emailState.inputText}</p> */}
+        <p>{emailState.errMessage}</p>
+        {/* <FormValidation formType="EMAIL" inputText={emailState.inputText}/> */}
         <input type="password" value={passwordState.inputText} onChange={(event)=>handleInputChange(event,updatePasswordState)}/>
-        <p>{passwordState.inputText}</p>
+        {/* <p>{passwordState.inputText}</p> */}
+        <p>{passwordState.errMessage}</p>
         <input type="password" value={passwordConfirmState.inputText} onChange={(event)=>handleInputChange(event,updatePasswordConfirmState)}/>
-        <p>{passwordConfirmState.inputText}</p>
+        {/* <p>{passwordConfirmState.inputText}</p> */}
+        <p>{passwordConfirmState.errMessage}</p>
     </form>);
 };
 
